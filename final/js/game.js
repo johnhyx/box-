883c8b617de544863e619a7747ec094cafcd3205
john_hyx <john_hyx@163.com>
2018-06-07 18:58:40 +0800

    完成游戏的基本功能，消行，计时，得分

--- final/js/game.js
+++ final/js/game.js
@@ -1,7 +1,11 @@
 var Game = function () {
     //dom元素
     var gameDiv;
-    var nextDIv;
+    var nextDiv;
+    var timeDiv;
+    var scoreDiv;
+    //保留分数
+    var score = 0;
     //游戏矩阵
     var gameData = [
         [0,0,0,0,0,0,0,0,0,0],
@@ -153,19 +157,109 @@ var clearData = function () {
             refreshDiv(gameData, gameDivs);
         }
     };
+//方块到达底部，将它固定
+    var fixed = function () {
+        for(var i = 0; i<cur.data.length; i++ ){
+            for(var j = 0; j<cur.data[0].length; j++){
+                if(check(cur.origin, i, j)){
+                    if(gameData[cur.origin.x + i][cur.origin.y + j] == 2){
+                        gameData[cur.origin.x + i][cur.origin.y + j] = 1;
+                    }
+                }
+            }
+        }
+        refreshDiv(gameData, gameDivs);
+    };
 
+    //实现消行
+    var checkClear = function () {
+        var line = 0;
+        for(var i = gameData.length - 1; i>=0; i--){
+            var clear = true;
+            for(var j = 0; j<gameData[0].length; j++){
+                if(gameData[i][j] != 1){
+                    clear = false;
+                    break;
+                }
+            }
+            if(clear){
+                line = line + 1;
+                for(var m = i; m>0; m--){
+                    for(var n=0; n<gameData[0].length; n++){
+                        gameData[m][n] = gameData[m-1][n];
+                    }
+                }
+                for(var n=0; n<gameData[0].length; n++){
+                    gameData[0][n] = 0;
+                }
+                i++;
+            }
+        }
+        return line;
+    };
+    //使用下一个方块，生成在游戏区
+    var performNext = function (type, dir) {
+        cur = next;
+        setData();
+        next = SquareFactory.prototype.make(type, dir)
+        refreshDiv(gameData, gameDivs);
+        refreshDiv(next.data, nextDivs)
+    };
+    
+    //检查游戏是否结束
+    var checkGameOver = function () {
+        var gameOver = false;
+        for(var i=0; i<gameData[0].length; i++ ){
+            if(gameData[1][i] == 1){
+                gameOver = true;
+            }
+        }
+        return gameOver;
+    };
+
+    //设置已用时间
+    var setTime = function (time) {
+        timeDiv.innerHTML = time;
+    };
+
+    //设置已经得分
+    var addScore = function (line) {
+        var s = 0;
+        switch (line){
+            case 1:
+                s = 10;
+                break;
+            case 2:
+                s = 30;
+                break;
+            case 3:
+                s = 60;
+                break;
+            case 4:
+                s = 100;
+                break;
+            case 5:
+                s = 150;
+                break;
+            default:
+                break;
+        }
+        score = score + s;
+        scoreDiv.innerHTML = score;
+    };
 //初始化
-    var init = function (doms) {
+    var init = function (doms, type, dir) {
         gameDiv = doms.gameDiv;
         nextDiv = doms.nextDiv;
-        cur = SquareFactory.prototype.make(4,2);
-        next = SquareFactory.prototype.make(4,3);
+        timeDiv = doms.timeDiv;
+        scoreDiv = doms.scoreDiv;
+        next = SquareFactory.prototype.make(type, dir);
         initDiv(gameDiv, gameData, gameDivs);
         initDiv(nextDiv, next.data, nextDivs);
-        cur.origin.x = 6;
-        cur.origin.y = 6;
-        setData();
-        refreshDiv(gameData, gameDivs);
+        // cur.origin.x = 6;
+        // cur.origin.y = 6;
+        // setData();
+        // refreshDiv(gameData, gameDivs);
         refreshDiv(next.data, nextDivs);
     };
     //到处初始化API
@@ -174,6 +268,12 @@ var clearData = function () {
     this.left = left;
     this.right = right;
     this.rotate = rotate;
+    this.fixd = fixed;
+    this.performNext = performNext;
+    this.checkClear = checkClear;
+    this.checkGameOver = checkGameOver;
+    this.setTime = setTime;
+    this.addScore = addScore;
     this.fall = function () {
         while (down());
     }
